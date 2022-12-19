5766709e0ad80c969b8a6d032e5f828ee495bdff
Simon <backstrom94+github@gmail.com>
2022-12-19 18:42:31 +0100

    another tiny bugfix

--- src/components/MovieListSection.jsx
+++ src/components/MovieListSection.jsx
@@ -70,38 +70,42 @@ function MovieListSection({ movies, error, loading, sectionId, title }) {
 
   // Render if successful fetch
   if (movies) {
-    return (
-      <section className="movie-list-section">
-        <h2 className="section-title">{title}</h2>
-        <div className="movie-list-controls">
-          {movies && movies.results.length > columns ? (
-            <>
-              <button
-                onClick={() => handleScrollButtonsClick(-1)}
-                className={`scroll-btn scroll-btn__left ${atSide === "left" ? "disabled" : ""}`}
-                aria-label="Scroll movie list left"
-              >
-                {/* Purely decorative icon. Buttons are labeled */}
-                <FaChevronLeft aria-hidden="true" />{" "}
-              </button>
-              <button
-                onClick={() => handleScrollButtonsClick(1)}
-                className={`scroll-btn scroll-btn__right ${atSide === "right" ? "disabled" : ""}`}
-                aria-label="Scroll movie list right"
-              >
-                {/* Purely decorative icon. Buttons are labeled */}
-                <FaChevronRight aria-hidden="true" />
-              </button>
-            </>
-          ) : null}
-          <div ref={movieListRef} onScroll={handleScrollEvent} className="movie-list">
-            {movies.results.map((movie, index) => {
-              return <MovieCard key={movie.id} movie={movie} index={index} sectionId={sectionId} />;
-            })}
+    if (movies.results) {
+      return (
+        <section className="movie-list-section">
+          <h2 className="section-title">{title}</h2>
+          <div className="movie-list-controls">
+            {movies.results.length > columns ? (
+              <>
+                <button
+                  onClick={() => handleScrollButtonsClick(-1)}
+                  className={`scroll-btn scroll-btn__left ${atSide === "left" ? "disabled" : ""}`}
+                  aria-label="Scroll movie list left"
+                >
+                  {/* Purely decorative icon. Buttons are labeled */}
+                  <FaChevronLeft aria-hidden="true" />{" "}
+                </button>
+                <button
+                  onClick={() => handleScrollButtonsClick(1)}
+                  className={`scroll-btn scroll-btn__right ${atSide === "right" ? "disabled" : ""}`}
+                  aria-label="Scroll movie list right"
+                >
+                  {/* Purely decorative icon. Buttons are labeled */}
+                  <FaChevronRight aria-hidden="true" />
+                </button>
+              </>
+            ) : null}
+            <div ref={movieListRef} onScroll={handleScrollEvent} className="movie-list">
+              {movies.results.map((movie, index) => {
+                return (
+                  <MovieCard key={movie.id} movie={movie} index={index} sectionId={sectionId} />
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    }
   }
 }
 
